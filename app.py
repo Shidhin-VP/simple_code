8b2cb76bdb4e9a0b60d1e2f91b8b744cf46c0193
Shidhin Varghese Philip <ShidhinVarghesePhilip98@gmail.com>
2025-10-06 14:36:21 -0500

    Updated

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pyperclip
 
 # Define the text you want to display
-text_to_copy = "This is the text you can copy to your clipboard!"
+text_to_copy = "dc23aa0f2333267cda6be7965f67814c"
 
 # Display the text in a text area (optional for visual appeal)
 st.text_area("Text to Copy", value=text_to_copy, height=150)
