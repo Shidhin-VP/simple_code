f37f79f853dbee90f0290addab49f5436457305e
Shidhin Varghese Philip <ShidhinVarghesePhilip98@gmail.com>
2025-10-06 14:40:09 -0500

    Updated

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pyperclip
 
 # Define the text you want to display
-text_to_copy = "dc23aa0f2333267cda6be7965f67814c"
+text_to_copy = """dc23aa0f2333267cda6be7965f67814c 
+
+or can just copy the key by copying and pasting normally as we do"""
 
 # Display the text in a text area (optional for visual appeal)
 st.text_area("Text to Copy", value=text_to_copy, height=150)
