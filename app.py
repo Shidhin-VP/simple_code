1360f681640f8d52b48b855abb9e1af3b47adb44
Shidhin Varghese Philip <ShidhinVarghesePhilip98@gmail.com>
2025-10-06 14:58:41 -0500

    Updated

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 import streamlit as st
 import pyperclip
 
-# Define the text you want to display
 text_to_copy = """dc23aa0f2333267cda6be7965f67814c 
 
 or just copy the key by copying and pasting normally as we do The button has some issues. :)"""
 
-# Display the text in a text area (optional for visual appeal)
 st.text_area("Text to Copy", value=text_to_copy, height=150)
 
-# Button to copy the text
 if st.button("Copy Text"):
-    # Copy the text to clipboard
     pyperclip.copy(text_to_copy)
     st.success("Text copied to clipboard!")
